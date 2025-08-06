28f9338ba36a3feafba4007a30fb8c805931ceaf
Cyrene <m16603557170@163.com>
2025-08-06 17:37:47 +0800

    day515redis的配置和常用操作

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -47,7 +47,7 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
             log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
             Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
-            log.info("当前员工id：", empId);
+            log.info("当前员工id：{}", empId);
             //将令牌中提取的id存放在本线程工具类里
             BaseContext.setCurrentId(empId);
             //3、通过，放行

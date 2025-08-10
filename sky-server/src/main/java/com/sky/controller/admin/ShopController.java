7af847bf0217367e11e3dd2d66cf0171d2ce0ef0
Cyrene <m16603557170@163.com>
2025-08-10 11:36:41 +0800

    day515修改查询店铺营业状态(未完成)

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,38 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequestMapping("/admin/shop")
+public class ShopController {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    /**
+     * 设置店铺的营业状态
+     */
+    @PutMapping("/{status}")
+    public Result setStatus(@PathVariable Integer status) {
+        log.info("设置店铺的营业状态为：{}",status==1 ? "营业中":"打烊中");
+        redisTemplate.opsForValue().set("SHOP_STATUS", status);
+        return Result.success();
+    }
+
+    /**
+     * 获取店铺营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get("SHOP_STATUS");
+        log.info("获取到的店铺状态为：{}",status==1? "营业中":"打烊中");
+        return Result.success(status);
+
+    }
+
+}

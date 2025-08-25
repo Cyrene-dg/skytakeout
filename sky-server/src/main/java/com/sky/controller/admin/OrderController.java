758c39494bd2b868d582ef55818ef598eb787eae
Cyrene <m16603557170@163.com>
2025-08-26 00:42:40 +0800

    day8,用户下单功能

---
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -0,0 +1,10 @@
+package com.sky.controller.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("adminOrderController")
+@RequestMapping("/admin/order")
+public class OrderController {
+    
+}
